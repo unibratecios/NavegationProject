15cab6ca46f5a1b96c06a95d89c5a0adb44f2cdc
Treinamento <treinamento@Treinamento1-Maquina2-2.local>
2017-07-29 13:29:58 -0300

    Primeiro commit

---
+++ NavegationProject/UltimaViewController.h
@@ -0,0 +1,13 @@
+//
+//  UltimaViewController.h
+//  NavegationProject
+//
+//  Created by Treinamento on 29/07/17.
+//  Copyright © 2017 Treinamento. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UltimaViewController : UIViewController
+
+@end
--- NavegationProject/UltimaViewController.m
+++ NavegationProject/UltimaViewController.m
@@ -1,43 +1,41 @@
 //
-//  ViewController.m
+//  UltimaViewController.m
 //  NavegationProject
 //
 //  Created by Treinamento on 29/07/17.
 //  Copyright © 2017 Treinamento. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "UltimaViewController.h"
 
-@interface ViewController ()
+@interface UltimaViewController ()
 
 @end
 
-@implementation ViewController
+@implementation UltimaViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view.
 }
-
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
+/*
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    if([segue.identifier isEqualToString:@"segueAzul"]){
-    
-    
-    }else if([segue.identifier isEqualToString:@"segueAmarelo"]){
-        
-        
-    }else{
-    }
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
 }
-
+*/
+- (IBAction)backToRoot:(id)sender {
+    [self.navigationController popToRootViewControllerAnimated:YES];
+    
+}
 
 @end
